9b9e7d26ec9db84225b88d11fd780ecc480796f6
sandeepshe <sandeep.shenoy@citrix.com>
2020-08-30 08:46:55 -0400

    added spinner

--- src/lib/angular-components.module.ts
+++ src/lib/angular-components.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { AngularComponentsComponent } from './angular-components.component';
 import { SdsSpinnerComponent } from './components/sds-spinner/sds-spinner.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
       AngularComponentsComponent, 
       SdsSpinnerComponent],
   imports: [
+    MatProgressSpinnerModule
   ],
   exports: [
       AngularComponentsComponent,
